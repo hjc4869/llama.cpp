8646012bcd9694aa17214b7d33dcc41a5450969e
David Huang <1969802+hjc4869@users.noreply.github.com>
2025-05-24 12:54:45 +0800

    Support gfx*-generic targets

--- ggml/src/ggml-cuda/vendors/hip.h
+++ ggml/src/ggml-cuda/vendors/hip.h
@@ -198,11 +198,12 @@
 #endif // defined(__GFX11__)
 
 #if defined(__gfx1030__) || defined(__gfx1031__) || defined(__gfx1032__) || defined(__gfx1033__) || \
-    defined(__gfx1034__) || defined(__gfx1035__) || defined(__gfx1036__) || defined(__gfx1037__)
+    defined(__gfx1034__) || defined(__gfx1035__) || defined(__gfx1036__) || defined(__gfx1037__) || \
+    defined(__gfx10_3_generic__)
 #define RDNA2
 #endif
 
-#if defined(__gfx1010__) || defined(__gfx1012__)
+#if defined(__gfx1010__) || defined(__gfx1012__) || defined(__gfx10_1_generic__)
 #define RDNA1
 #endif // defined(__gfx1010__) || defined(__gfx1012__)
 
